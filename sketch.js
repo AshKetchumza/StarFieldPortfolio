e163e67cae39250cec5d0604141a05f980bdfe75
Ashley Sanders <ashleymsanders2@gmail.com>
2017-11-16 10:59:26 +0200

    Added the best background track of any website in the world, and moved custom styles into style.css

--- sketch.js
+++ sketch.js
@@ -5,11 +5,22 @@ var stars = [];
 var speed;
 
 function setup() {
+  //Comment out for local running    
+  //song = loadSound('assets/song.mp3');
+  masterVolume(0.5);
   createCanvas(window.innerWidth, window.innerHeight);
   for (var i = 0; i < 3000; i++) {
     techs[i] = new Tech();
     stars[i] = new Star();
   }    
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+  } else {
+    song.play();
+  }
 }
 
 function draw() {
@@ -20,8 +31,8 @@ function draw() {
     techs[i].update();
     techs[i].show();
   }
-  for (var j = 0; j < stars.length; j++) {
-    stars[j].update();
-    stars[j].show();
+  for (var i = 0; i < techs.length; i++) {
+    stars[i].update();
+    stars[i].show();
   }
 }
